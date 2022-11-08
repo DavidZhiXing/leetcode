09e6db60c723551aaab85afd41f7d71a6364628b
star <weizhixing@genomics.com>
2022-11-08 17:33:24 +0800

    test fizzbuzz

--- fizzbuzz.cs
+++ fizzbuzz.cs
@@ -1,9 +1,9 @@
-Given a number n, for each integer i in the range from 1 to n inclusive, print one value per line as follows:
-
-If i is a multiple of both 3 and 5, print FizzBuzz.
-If i is a multiple of 3 (but not 5), print Fizz.
-If i is a multiple of 5 (but not 3), print Buzz.
-If i is not a multiple of 3 or 5, print the value of i.
+// Given a number n, for each integer i in the range from 1 to n inclusive, print one value per line as follows:
+// 
+// If i is a multiple of both 3 and 5, print FizzBuzz.
+// If i is a multiple of 3 (but not 5), print Fizz.
+// If i is a multiple of 5 (but not 3), print Buzz.
+// If i is not a multiple of 3 or 5, print the value of i.
 
 public class Solution {
     public IList<string> FizzBuzz(int n) {
@@ -19,7 +19,7 @@ public class Solution {
                 result.Add(i.ToString());
             }
         }
-        foreach (var item in result) {
+            foreach (var item in result) {
             Console.WriteLine(item);
         }
     }
